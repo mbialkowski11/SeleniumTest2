a3b18d0f3f96f28a9cec87efb9164e16b8a5dc6f
Michał Białkowski <michal.bialkowski11@gmail.com>
2023-10-08 11:03:39 +0200

    DataProvider

--- src/test/java/pl/testeroprogramowania/FourthTest.java
+++ src/test/java/pl/testeroprogramowania/FourthTest.java
@@ -1,5 +1,6 @@
 package pl.testeroprogramowania;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FourthTest {
@@ -21,4 +22,19 @@ public class FourthTest {
 
         System.out.println("I am third test");
     }
+
+    @Test(dataProvider = "data")
+    public void dpTest(String val, String number) {
+
+        System.out.println(val);
+        System.out.println(number);
+    }
+
+
+    @DataProvider(name = "data")
+    public Object[][] dataProvider() {
+        return new Object[][] {{"I am first test","First"}, {"I am second test","Second"}, {"I am third test","Third"}};
+
+    }
+
 }
--- src/test/java/pl/testeroprogramowania/SampleTestListener.java
+++ src/test/java/pl/testeroprogramowania/SampleTestListener.java
@@ -1,8 +1,15 @@
 package pl.testeroprogramowania;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SampleTestListener implements ITestListener {
 
@@ -14,7 +21,18 @@ public class SampleTestListener implements ITestListener {
     }
 
     public void onTestFailure(ITestResult iTestResult) {
-        System.out.println("I am taking screenshot");
+        //System.out.println("I am taking screenshot");
+
+        WebDriver driver = DriverFactory.getDriver();
+        int randomNumber = (int) (Math.random()*1000);
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File before = screenshot.getScreenshotAs(OutputType.FILE);
+        String fileName = "failedTest" + randomNumber + ".png";
+        try {
+            FileUtils.copyFile(before,new File("src/test/resources/" + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onTestSkipped(ITestResult iTestResult) {
